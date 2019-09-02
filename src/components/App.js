04dd9563bffb5a76b0345f3cb1aa9ae5eb045708
Guneet Singh <guneetnagia24@gmail.com>
2019-09-03 02:31:33 +0530

    Front End - Clothing

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {HomePage} from '../pages/home-page/home-page.component'
+
+export default class App extends React.Component {
+    render(){
+        return (
+            <HomePage />
+        );
+    }
+}
