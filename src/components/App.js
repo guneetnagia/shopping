1b8fc2a4eea70972769481ecb0431a5d2cea8838
Guneet Singh <guneetnagia24@gmail.com>
2019-09-06 01:02:17 +0530

    adding routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {HomePage} from '../pages/home-page/home-page.component'
+import { HomePage } from '../pages/home-page/home-page.component';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default class App extends React.Component {
-    render(){
+    render() {
         return (
-            <HomePage />
+            <Router>
+                <div>
+                    <switch>
+                        <Route path="/" component={HomePage} />
+                        <Route path="/about" component={About} />
+                    </switch>
+                </div>
+            </Router>
         );
     }
+}
+
+function About(){
+    return <h2>About</h2>
 }
