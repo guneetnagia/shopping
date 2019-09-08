44a690ef9d2ea4b956f6c5a0ba05b5cdd37c7fc2
Guneet Singh <guneetnagia24@gmail.com>
2019-09-09 02:22:52 +0530

    Updates

---
+++ precache-manifest.d2147f616583abc8b82fdce21b8c8b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dfdc418d59cb927fc12530073f2d38f",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "db576d29afd7464d8742",
+    "url": "/shopping/static/css/main.80fe696c.chunk.css"
+  },
+  {
+    "revision": "2e4f6e7ab5ec2bafd50b",
+    "url": "/shopping/static/js/2.ee0d43ce.chunk.js"
+  },
+  {
+    "revision": "db576d29afd7464d8742",
+    "url": "/shopping/static/js/main.12afd525.chunk.js"
+  },
+  {
+    "revision": "373f8e11ea6b435f939b",
+    "url": "/shopping/static/js/runtime~main.ffbfa2d9.js"
+  },
+  {
+    "revision": "5b6cf2e1b199e80db2ffb66551120b24",
+    "url": "/shopping/static/media/crown.5b6cf2e1.svg"
+  }
+]);
