d875d94518ee1f7a117d3196f392f03e5cd661b4
Guneet Singh <guneetnagia24@gmail.com>
2020-03-31 17:20:55 +0530

    folder structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HomePage from './pages/homepage/homepage.component';
+
+ReactDOM.render(<HomePage />,document.getElementById('root'));
