efc80750bf7a2a08644d299d28ff582218dbb47f
Guneet Singh <guneetnagia24@gmail.com>
2020-04-01 20:07:53 +0530

    Preview Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import '../src/App.css';
 
 import { Route, Switch } from 'react-router-dom';
@@ -9,6 +10,7 @@ const App = () => {
         <div>
             <Switch>
                 <Route exact path="/" component={HomePage} />
+                <Route exact path="/shop" component={ShopPage} />
             </Switch>
         </div>
     )
