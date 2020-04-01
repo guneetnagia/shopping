d8ab791415d98300b7a475279b2cbee97442c8ac
Guneet Singh <guneetnagia24@gmail.com>
2020-04-01 18:41:07 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import '../src/App.css';
 
+import { Route, Switch } from 'react-router-dom';
+
 const App = () => {
     return (
-        <HomePage />
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+            </Switch>
+        </div>
     )
 }
-
 export default App;
