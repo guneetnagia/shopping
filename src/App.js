0e209b3e09196412815f91e82b88da76db265d4c
Guneet Singh <guneetnagia24@gmail.com>
2020-04-17 20:53:21 +0530

    adding redux

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import '../src/App.css';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import { Route, Switch } from 'react-router-dom';
 
@@ -12,7 +13,8 @@ const App = () => {
             <Header />
             <Switch>
                 <Route exact path="/" component={HomePage} />
-                <Route exact path="/shop" component={ShopPage} />
+                <Route path="/shop" component={ShopPage} />
+                <Route exact path="/signIn" component={SignInAndSignUp} />
             </Switch>
         </div>
     )
