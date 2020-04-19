4ff4d81ff50f7915f44b07c095190ccae84bb8d5
Guneet Singh <guneetnagia24@gmail.com>
2020-04-20 01:46:28 +0530

    Redirect on sign in

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import {UserActionTypes} from './user.type';
+
 const INIT_STATE = {
     currentUser: null
 }
 export const UserReducer = (state = INIT_STATE, action) => {
     const { type, payload } = action;
     switch (type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: payload
