134eda419b4e29fa3f4b30be50a61c43f7ea99fe
Guneet Singh <guneetnagia24@gmail.com>
2020-04-20 01:11:47 +0530

    mapDispatchToProps

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import { auth } from '../../firebase/firebase.utils';
-
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-
 import './header.styles.scss';
+import { connect } from 'react-redux';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
@@ -24,12 +22,15 @@ const Header = ({ currentUser }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
-      )}
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )}
     </div>
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser : state.user.currentUser
+})
+export default connect(mapStateToProps)(Header);
